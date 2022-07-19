d516a5b0df741c2d46ebf8c2a8719dcdbc3b794c
Emran Hossen <92653363+emranhossen26@users.noreply.github.com>
2022-07-19 23:53:10 +0600

    Create 51.class and object.cpp

---
+++ 51.class and object.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class student
+{
+    public:
+    int id;
+    double gpa;
+
+};
+
+int main()
+{
+ 
+ student alim,shakib;
+
+ alim.id=40;
+ alim.gpa=3.88;
+
+ cout<<alim.gpa<<"  "<<alim.id;
+
+cout<<endl;
+
+ shakib.id=64;
+ shakib.gpa=3.96;
+
+ cout<<shakib.gpa<<"  "<<shakib.id;
+
+
+}
